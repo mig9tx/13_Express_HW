3b5534d40a36b714e19299ab97cc551ec809cd0e
mig9tx <migue.trujillo@utexas.edu>
2018-11-11 23:50:09 -0600

    logic still not returning what is required, added default friends list

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # 13_Express_HW
+
+This application that allow the user to answer a short survey and upon completion allows the user to be matched with another user in the server.
+
+The available users to be matched are saved within an apiRoute file and obtained using an AJAX call.
+
+Once the user is matched, their results are POSTed to the friends file and stored. This will in turn, allow them to be matched with future users that take the survey.
+
+## This application utilizes the following:
+
+node.js for the routing of the files and information.
+
+javaScript/jQuery for the logic and DOM manipulation.
+
+Heroku for live site deployment.
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,99 @@
-const friends =  [];
+const friends = [{
+        "name": "Joey",
+        "image": "https://themodernjedi.files.wordpress.com/2012/08/joey-shocked.jpg",
+        "scores": [
+            "5",
+            "4",
+            "3",
+            "4",
+            "3",
+            "2",
+            "4",
+            "2",
+            "3",
+            "5"
+        ]
+    },
+    {
+        "name": "Ross",
+        "image": "https://giphy.com/embed/4qmcuu67Hxx0Q",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "5",
+            "4",
+            "4",
+            "3",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Chandler",
+        "image": "https://gph.is/1eRxhJy",
+        "scores": [
+            "2",
+            "3",
+            "4",
+            "3",
+            "4",
+            "3",
+            "3",
+            "4",
+            "4",
+            "3"
+        ]
+    },
+    {
+        "name": "Phoebe",
+        "image": "https://gph.is/1FwRLhd",
+        "scores": [
+            "3",
+            "1",
+            "5",
+            "5",
+            "3",
+            "3",
+            "5",
+            "3",
+            "1",
+            "5"
+        ]
+    },
+    {
+        "name": "Monica",
+        "image": "http://www.comedycentral.co.uk/sites/default/files/styles/image-w-900-scale/public/cc_uk/galleries/2016/08/25/15.gif?itok=IUiV-V0p",
+        "scores": [
+            "5",
+            "3",
+            "4",
+            "1",
+            "2",
+            "5",
+            "3",
+            "5",
+            "4",
+            "3"
+        ]
+    },
+    {
+        "name": "Rachel",
+        "image": "https://gph.is/1ICT7gu",
+        "scores": [
+            "4",
+            "1",
+            "5",
+            "3",
+            "5",
+            "2",
+            "2",
+            "2",
+            "3",
+            "4"
+        ]
+    }
+];
 
 module.exports = friends;
-
-
